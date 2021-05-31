28ba665b2d7466925d496a4fa3cd9312985a0c30
daklo91 <coffeedog91@gmail.com>
2021-05-31 12:49:27 +0200

    Add NavLinks component

---
+++ components/NavLinks.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <p>Pokemon</p>
+    <p>Moves</p>
+    <p>Map</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container p {
+  font-family: var(--press-start-2p);
+}
+</style>
