d0f9d9f1d47d4ae63557c374e4bd53a4790a172b
daklo91 <coffeedog91@gmail.com>
2021-05-31 13:01:17 +0200

    Add style to navlinks (function later)

--- components/NavLinks.vue
+++ components/NavLinks.vue
@@ -11,7 +11,15 @@ export default {}
 </script>
 
 <style scoped>
+.container {
+  margin: auto;
+  max-height: 20px;
+}
+
 .container p {
   font-family: var(--press-start-2p);
+  color: var(--dark-gray);
+  font-size: 18px;
+  margin-left: 30px;
 }
 </style>
