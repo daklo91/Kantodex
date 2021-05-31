07f42a7c851051942ddfc36f88c150c197d77c69
daklo91 <coffeedog91@gmail.com>
2021-05-31 12:49:14 +0200

    Add width and spacing

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -2,24 +2,22 @@
   <div class="container">
     <div class="logo-and-search">
       <Logo />
-      <FuzzySearch />
+      <FuzzySearch class="fuzzy-search" />
     </div>
-    <div class="nav-links">
-      <p>Pokemon</p>
-      <p>Moves</p>
-      <p>Map</p>
-    </div>
+    <NavLinks class="nav-links" />
   </div>
 </template>
 
 <script>
 import Logo from '@/components/Logo.vue'
 import FuzzySearch from '@/components/FuzzySearch.vue'
+import NavLinks from '@/components/NavLinks.vue'
 
 export default {
   components: {
     Logo,
     FuzzySearch,
+    NavLinks,
   },
 }
 </script>
@@ -28,10 +26,16 @@ export default {
 .container {
   display: flex;
   height: 90px;
+  max-width: 1200px;
+  margin: auto;
 }
 
 .container .logo-and-search {
   display: flex;
+}
+
+.container .logo-and-search .fuzzy-search {
+  margin-left: 40px;
 }
 
 .nav-links {
